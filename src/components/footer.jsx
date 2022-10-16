c005f7a796ba8192fc43a60801b3df2f3bbfce6e
yakir <danielyakir01@gmail.com>
2022-10-16 11:03:06 +0300

    css changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer className="text-center text-light mt-auto mb-3">
-            <div className="text-center p-3">
+        <footer className="text-center text-light mt-auto mb-3 bg-warning">
+            <div className="text-center text-dark p-3">
                 © 2022 Copyrights belongs to Yakir Daniel
             </div>
         </footer>
